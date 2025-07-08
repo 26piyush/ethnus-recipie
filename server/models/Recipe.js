dcc6bbd9563a1fda4986e52f55034b8e32b9350a
piyushsom <88475486+26piyush@users.noreply.github.com>
2025-07-08 22:52:42 +0530

    Update Recipe.js

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
 
 const RecipeSchema = new mongoose.Schema({
-  name: String,
-  ingredients: [String],
-  cuisine: String
+  name: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: [String],
+    required: true,
+  },
+  cuisine: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,         // <-- Add this line
+    required: false,      // Optional unless you want to enforce it
+  }
 });
 
 module.exports = mongoose.model('Recipe', RecipeSchema);
